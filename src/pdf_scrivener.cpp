9d80a927527e477ddbc554a7a981de2f99e76975
Gabriel Okafor <gabeoak@outlook.com>
2023-08-06 16:38:54 +0100

    finally got the wizard setup working...moving onto the first context selection stuff blah blah blah

--- src/pdf_scrivener.cpp
+++ src/pdf_scrivener.cpp
@@ -40,37 +40,37 @@ std::unordered_map<char, int> charOccur;
 std::string badChars = "";
 
 // generic page class. I should probably put this in a header file
-class Page : public Fl_Window {
+class MyPage : public Fl_Group {
 public:
-    Page(int w, int h, const char* title = 0) : Fl_Window(w, h, title) {}
+    MyPage(int x, int y, int w, int h, const char* title = 0) : Fl_Group(x, y, w, h, title) {}
 };
 
 // window that lets you choose what to do with the bad characters
-class ChoicePage : public Page {
+class ChoicePage : public MyPage {
 private:
     Fl_Box* heyHowUDoin;
 
 public:
-    ChoicePage(int w, int h, const char* title = 0) : Page(w, h, title) {
+    ChoicePage(int x, int y, int w, int h, const char* title = 0) : MyPage(x, y, w, h, title) {
         heyHowUDoin = new Fl_Box(450, 350, 100, 100, "You look fine as a newly minted dime"); // Centered HelloWorld box
         end();
     }
 };
 
 
-class OpenPDFPage : public Page {
+class OpenPDFPage : public MyPage {
 private:
-    Fl_Button* btn;
-    Fl_Multiline_Output* out;
+    Fl_Button* loadBtn;
+    Fl_Multiline_Output* badOut;
     Fl_Button* nextBtn;
 
 public:
-    OpenPDFPage(int w, int h, const char* title = 0) : Page(w, h, title) {
-        btn = new Fl_Button(50, 50, 150, 40, "Open PDF");
-        out = new Fl_Multiline_Output(50, 200, 900, 600, "");    
-        btn->callback(loadPDF, out);  
-
+    OpenPDFPage(int x, int y, int w, int h, const char* title = 0) : MyPage(x, y, w, h, title) {
+        loadBtn = new Fl_Button(50, 50, 150, 40, "Open PDF");
+        badOut = new Fl_Multiline_Output(50, 200, 900, 600, "");
         nextBtn = new Fl_Button(250, 50, 150, 40, "Next");
+
+        loadBtn->callback(loadPDF, badOut);
         nextBtn->callback(goToChoicePage);  
 
         end();  
@@ -81,100 +81,125 @@ public:
         const char* thisPDF = fl_file_chooser("Select a PDF", "*.pdf", NULL); // open file chooser 
         // if it's a valid file
         if (thisPDF) {
-            std::cout << "Selected PDF: " << thisPDF << std::endl; // print the filepath to terminal
+            // try to load it as a PDF
+            std::cout << "Selected PDF: " << thisPDF << std::endl;
             std::unique_ptr<poppler::document> popplerDoc(poppler::document::load_from_file(thisPDF)); // load the PDF
-            
+
+
             // if the loaded PDF is valid, then parse it and extract text
             if (popplerDoc) {
-                int numPages = popplerDoc->pages(); // get the number of pages in the PDF
-
+                std::cout << "Processing PDF" << std::endl;
+                int numPages = (popplerDoc->pages()); // get the number of pages in the PDF
+                std::cout << "Number of pages: " << numPages << std::endl;
                 // for every page in the PDF
-                for (int page_dex = 0; page_dex < numPages; ++page_dex) {
-                    std::unique_ptr<poppler::page> pageIt(popplerDoc->create_page(page_dex)); // init a pointer to the page beginning
+                for (int page_dex = 0; page_dex < numPages; page_dex++) {
+                    std::cout << "Processing page " << page_dex << std::endl;
+                    std::unique_ptr<poppler::page> pageIt(popplerDoc->create_page(page_dex)); // create a unique pointer to the page
 
                     // if the pointer is valid
                     if (pageIt) {
                         poppler::byte_array byteArray = pageIt->text().to_utf8(); // get data in utf8 encoding
                         std::string text(byteArray.begin(), byteArray.end()); // convert bytes to string
                         pageList.push_back(text); // push to the pageList vector
-                    } 
+                    }
+                    pageIt.reset(); // reset the page pointer
+
+
                 }
-            } 
+                std::cout << "Finished Processing PDF" << std::endl;
+                // for every page
+                for(int pageInt = 0; pageInt < pageList.size(); pageInt++) {
+                    std::cout << "Processing page " << pageInt << std::endl;
+                    bool leadingWhiteSpace = true; // to skip leading whitespaces
 
-            std::ofstream outFile("pageOut.txt"); // open a .txt file to write text to (DEBUGGING)
-            outFile << char(0xEF) << char(0xBB) << char(0xBF); // assert utf8 encoding
+                    // for every character in the page
+                    for(int charIt = 0; charIt < pageList[pageInt].size(); charIt++) {
+                        // grab the character
+                        char thisChar = pageList[pageInt][charIt]; 
 
-            // same old story
-            std::ofstream badtxt("badChars.txt", std::ios::binary);
-            badtxt << char(0xEF) << char(0xBB) << char(0xBF); 
+                        // if character is a newline, reset leading whitespace 
+                        if(leadingWhiteSpace && thisChar == ' ') {
+                            continue; // if it's a leading whitespace, skip it
+                        } else if(thisChar == '\n'){
+                            // if it's a newline, reset leading whitespace
+                            leadingWhiteSpace = true;
 
-            // for every page
-            for(int pageIt = 0; pageIt < pageList.size(); ++pageIt) {
-                bool leadingWhiteSpace = true; // to skip leading whitespaces
-
-                // for every character in the page
-                for(int charIt = 0; charIt < pageList[pageIt].size(); ++charIt) {
-                    char thisChar = pageList[pageIt][charIt]; // grab the character
-
-                    // if character is a newline, reset leading whitespace 
-                    if(leadingWhiteSpace && thisChar == ' ') {
-                        continue; // if it's a leading whitespace, skip it
-                    } else if(thisChar == '\n'){
-                        // if it's a newline, reset leading whitespace
-                        leadingWhiteSpace = true;
-
-                    } else {
-                        // otherwise, it's not a leading whitespace
-                        leadingWhiteSpace = false;
-
-                        // if it's not a printable (good) character
-                        if(printable.find(thisChar) == std::string::npos) {
-                            if(badChars.find(thisChar) == std::string::npos) {
-                                badChars += thisChar; // add to bad character list
-                            }
-                        charOccur[thisChar]++; // add to number of character occurrences
                         } else {
-                            // otherwise, it's a printable character. do the same thing as above
-                            charOccur[thisChar]++;
-                        }
-                    } 
+                            // otherwise, it's not a leading whitespace
+                            leadingWhiteSpace = false;
+
+                            // if it's not a printable (good) character
+                            if(printable.find(thisChar) == std::string::npos) {
+                                if(badChars.find(thisChar) == std::string::npos) {
+                                    badChars += thisChar; // add to bad character list
+                                }
+                            charOccur[thisChar]++; // add to number of character occurrences
+                            } else {
+                                // otherwise, it's a printable character. do the same thing as above
+                                charOccur[thisChar]++;
+                            }
+                        } 
+                    }
+                    
                 }
+                // create ostream to announce number of bad characters
+                std::ostringstream os;
+                os << "This document has " << badChars.size() << " bad characters:\n\n";
+
+                // loop thru the charOccur map, append each bad character and its num occurrences
+                for(int i = 0; i < badChars.size(); ++i) {
+                    os << i << ' ' << badChars[i] << ", with: " << charOccur[badChars[i]] << " occurrences\n";
+                }
+
+                // container for multiline output
+                Fl_Multiline_Output* badOut = (Fl_Multiline_Output*)data;
+
+                // send bad character notification to the output
+                badOut->value(os.str().c_str());
+                badOut->redraw(); // redraw the output
+                std::cout << os.str() << std::endl; // print to console
+                std::cout << "Processing complete" << std::endl;
+            } else{
+                std::cout << "Invalid PDF" << std::endl;
             }
-
-            // create ostream to announce number of bad characters
-            std::ostringstream os;
-            os << "This document has " << badChars.size() << " bad characters:\n\n";
-
-            // loop thru the charOccur map, append each bad character and its num occurrences
-            for(int i = 0; i < badChars.size(); ++i) {
-                os << i << ' ' << badChars[i] << ", with: " << charOccur[badChars[i]] << " occurrences\n";
-            }
-
-            // container for multiline output
-            Fl_Multiline_Output* out = (Fl_Multiline_Output*)data;
-
-            // send bad character notification to the output
-            out->value(os.str().c_str());
-            out->redraw(); // redraw the output
-
-            std::cout << "Processing complete" << std::endl;
-
         } else {
             std::cout << "No PDF selected" << std::endl;
         }
-    }    
 
+        std::cout << "Finished Loading PDF" << std::endl;
+    }
     static void goToChoicePage(Fl_Widget* w, void* data) {
-        ((Fl_Window*)w->window())->hide(); // hide this window
-        ChoicePage choiceWin(1000, 800, "PDF Scrivener - Choice Page"); 
-        choiceWin.show(); // show the next window
-        Fl::run(); // run the next window
+        // tell the wizard to go to the next page
+        ((Fl_Wizard*)w->parent()->parent())->next();
+    }
+};
+
+class AppWizard : public Fl_Window {
+private:
+    Fl_Wizard* wizard;
+    OpenPDFPage* openPage;
+    ChoicePage* choicePage;
+
+public:
+    AppWizard(int w, int h, const char* title = 0) : Fl_Window(w, h, title){
+        wizard = new Fl_Wizard(0, 0, w, h);
+        openPage = new OpenPDFPage(0, 0, w, h, "PDF Scrivener - Open PDF");
+        choicePage = new ChoicePage(0, 0, w, h, "PDF Scrivener - Choice Page");
+
+        // add the pages to the wizard
+        wizard->add(openPage);
+        wizard->add(choicePage);
+
+        // hide all pages except the first
+        wizard->value(openPage);
+
+        wizard->end();
     }
 };
 
 
 int main(int argc, char** argv) {
-    OpenPDFPage openPDFWin(1000, 800, "PDF Scrivener - Open PDF");
-    openPDFWin.show(argc, argv);
+    AppWizard appWin(1000, 800, "PDF Scrivener");
+    appWin.show(argc, argv);
     return Fl::run();
 }
